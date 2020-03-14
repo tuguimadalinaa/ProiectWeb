cd1083b3d5823f6b559148d6080911999a8dac7f
Tugui Madalina <tuguimadalinaa@gmail.com>
2020-03-14 23:04:28 +0200

    added responsive design to index-about pages

--- JS/login.js
+++ JS/login.js
@@ -6,9 +6,15 @@ function goToIndex()
     let userName = document.getElementById("usernameInput").value;
     let password = document.getElementById("passwordInput").value;
     window.location.href= newPage;
-    alert(userName + "    " + password);
     return false;
     //https://stackoverflow.com/questions/16562577/how-can-i-make-a-button-redirect-my-page-to-another-page
     //https://stackoverflow.com/questions/3151436/how-can-i-get-the-current-directory-name-in-javascript
     //https://stackoverflow.com/questions/6109527/window-location-href-not-working-in-form-onsubmit
+}
+function goToSignUp()
+{
+    let pathName=window.location.pathname; 
+    let directory = pathName.substring(0, pathName.lastIndexOf('/'));
+    let newPage  = directory + "/signUp.html";
+    window.location.href= newPage;
 }
---
+++ JS/signUp.js
@@ -0,0 +1,11 @@
+function registerUser()
+{
+    let pathName=window.location.pathname; 
+    let directory = pathName.substring(0, pathName.lastIndexOf('/'));
+    let newPage  = directory + "/login.html";
+    let userName = document.getElementById("usernameInput").value;
+    let password = document.getElementById("passwordInput").value;
+    let passwordCheck = document.getElementById("passwordCheck").value;
+    window.location.href= newPage;
+    return false;
+}
