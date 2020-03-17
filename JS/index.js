95a54621443be12ab940498bab0debea483c19d0
Robert Frentz <frentzrobert@yahoo.com>
2020-03-17 11:07:01 +0200

    File Uploading Explorer

--- JS/index.js
+++ JS/index.js
@@ -11,5 +11,26 @@ function closeMenu()
 }
 function uploadAllDrivers()
 {
-    alert("pressed button");
+    let oneDrive = document.getElementById("one-drive");
+    let googleDrive = document.getElementById("google-drive");
+    let dropBox = document.getElementById("drop-box");
+    let allCloudMethods = document.getElementById("allCloudMethods");
+
+    var htmlString = "";
+    htmlString = "<input id='fileid' type='file' hidden/>";
+    googleDrive.insertAdjacentHTML('beforebegin',htmlString);
+
+    allCloudMethods.addEventListener('click', function(){
+        document.getElementById('fileid').click();
+    });
+    oneDrive.addEventListener('click', function(){
+        document.getElementById('fileid').click();
+    });
+    googleDrive.addEventListener('click', function(){
+        document.getElementById('fileid').click();
+    });
+    dropBox.addEventListener('click', function(){
+        document.getElementById('fileid').click();
+    });
+
 }
